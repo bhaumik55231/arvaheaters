5694cb6d9225af77ae218257845072e940e2a884
bhaumik55231 <bhaumik55231@gmail.com>
2019-08-03 01:09:48 -0400

    Adding grid view

--- index.js
+++ index.js
@@ -10,6 +10,20 @@ const main = () => {
             element.classList.add('active');
         });
     });
+
+    const slider = document.getElementById('silder');
+    const grid = document.getElementById('grid');
+    slider.addEventListener('click', () => {
+        slider.childNodes[0].classList.add('active');
+        grid.childNodes[0].classList.remove('active');
+        document.getElementById('productContent').innerHTML = productSlider();
+    });
+    grid.addEventListener('click', () => {
+        grid.childNodes[0].classList.add('active');
+        slider.childNodes[0].classList.remove('active');
+        document.getElementById('productContent').innerHTML = productGrid();
+    });
+    slider.dispatchEvent(new Event('click'));
 };
 
 const removeActiveClass = (className, activeClass) => {
@@ -18,3 +32,48 @@ const removeActiveClass = (className, activeClass) => {
         elm.classList.remove(activeClass);
     });
 };
+
+const productSlider = () => {
+    return `
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="./assets/mica_band.jpg" height="550px" alt="First slide">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Mica Band Heater Cover Type</h5>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="./assets/2.jpg" height="550px" alt="Second slide">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    `;
+}
+
+const productGrid = () => {
+    return `
+        <div class="row">
+            <div class="col-sm">
+                <img src="./assets/mica_band.jpg" class="image-border" height="200px" alt="First grid"></br>
+                Mica Band Heater Cover Type
+            </div>
+            <div class="col-sm">
+                <img src="./assets/2.jpg" class="image-border" height="200px" alt="Second grid"></br>
+            </div>
+            <div class="col-sm"></div>
+        </div>
+    `;
+}
